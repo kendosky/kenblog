fb101c1e34725459b6f4e924b72a55324043ea53
kendosky <kendothech1@gmail.com>
2022-03-15 02:49:38 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var darkbtn = document.getElementById("dark-btn");
+
+function settingsmenutoggle(){
+    settingsmenu.classList.toggle("settings-menu-height"); 
+}
+
+darkbtn.onclick = function(){
+    darkbtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+}
+
+if(localStorage.getItem("theme") =="light"){
+    darkbtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") =="dark"){
+    darkbtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+
+localStorage.setItem("dark,theme");
+localStorage.setItem("theme");
